0f86eea1760a1eadd28fbcb85225ae5b842eb8e5
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-04 10:41:38 +0300

    Some cool features for io

---
+++ lib/redmine_cli/helpers/input.rb
@@ -0,0 +1,84 @@
+require 'uri'
+
+require_relative 'output'
+
+module RedmineCLI
+  module Helpers
+    #
+    # Helpers for input
+    #
+    module Input
+      include Helpers::Output
+
+      #
+      # #ask with :limited_to set to url regexp
+      #
+      def ask_url(text, params = {})
+        params[:limited_to] = /\A#{URI.regexp(%w('http', 'https'))}\z/
+        ask(text, params)
+      end
+
+      #
+      # Asks user for something.
+      #
+      # Params:
+      # * `:default`
+      # * `:limited_to` - can be `Array<String>` or `Regexp`
+      # * `:required` - force user input until it's not empty
+      #
+      def ask(text, params = {})
+        default_set = params.key?(:default)
+        if params[:required]
+          fail('required + default options') if default_set
+          fail('required + array with limits') if params[:limited_to].is_a? Array
+        end
+
+        print_prompt_message(text, params)
+        input = read(params)
+
+        return params[:default] if default_set && input.empty?
+        input
+      end
+
+      private
+
+      def read(params)
+        return read_with_limitations(params) if params[:limited_to]
+        return read_until_not_empty if params[:required]
+
+        gets.chomp
+      end
+
+      def read_until_not_empty
+        loop do
+          input = gets.chomp
+          return input unless input.empty?
+
+          puts m(:error_input_required)
+        end
+      end
+
+      def read_with_limitations(params)
+        input = nil
+
+        loop do
+          input = gets.chomp
+          break if fit_in_limit?(input, params[:limited_to])
+          return params[:default] if input.empty? && params.key?(:default)
+
+          puts m(:error_try_again)
+        end
+
+        input
+      end
+
+      def fit_in_limit?(input, limit)
+        fail('limit should be Array or Regexp') unless limit.is_a?(Array) || limit.is_a?(Regexp)
+
+        return limit.include?(input) if limit.is_a? Array
+
+        limit =~ input
+      end
+    end
+  end
+end
