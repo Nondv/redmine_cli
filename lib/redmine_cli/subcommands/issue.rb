364223cd51c08c4e234b206f6d1347e9b2eef745
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-07 23:52:17 +0300

    add empty command `issue update`

--- lib/redmine_cli/subcommands/issue.rb
+++ lib/redmine_cli/subcommands/issue.rb
@@ -23,11 +23,13 @@ module RedmineCLI
       option :limit, aliases: ['-l'], type: :numeric, default: 5, desc: m('desc.issue.options.show.limit')
       def show(id)
         puts erb('issue/show', issue: Models::Issue.find(id), journals_limit: options[:limit])
-      #
-      # WARNING: it can be raised by associations in template
-      #
-      rescue ActiveResource::ResourceNotFound
+
+      rescue ActiveResource::ResourceNotFound # WARNING: it can be raised by associations in template
         puts m(:not_found)
+      end
+
+      desc 'update <id>', m('desc.issue.update')
+      def update(id)
       end
     end
   end
