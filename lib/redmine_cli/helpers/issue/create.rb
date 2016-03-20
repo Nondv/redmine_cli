6ce0cf4136d0f1caaaf44551b095140861b3d148
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-20 03:56:13 +0300

    `issue create` asks for subject

--- lib/redmine_cli/helpers/issue/create.rb
+++ lib/redmine_cli/helpers/issue/create.rb
@@ -14,11 +14,17 @@ module RedmineCLI
 
         def set_attributes
           set_tracker
+          set_subject
         end
 
         def set_tracker
           puts Unicode.upcase(m(:trackers)) + ':'
           @issue.tracker_id = ask_for_object(Models::Tracker.all).id
+        end
+
+        def set_subject
+          @issue.subject = ask m('commands.issue.create.enter_subject'),
+                               required: true
         end
       end
     end
