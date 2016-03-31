7f802f52a5e2799895b8f11805b8c29209726dc4
Dmitriy Non <non.dmitriy@gmail.com>
2016-04-01 01:18:08 +0300

    `project members`

--- lib/redmine_cli/exceptions.rb
+++ lib/redmine_cli/exceptions.rb
@@ -1,4 +1,5 @@
 module RedmineCLI
+  class ProjectNotFound < StandardError; end
   class UserNotFound < StandardError; end
   class BadInputTime < StandardError; end
   class EmptyList < StandardError; end
